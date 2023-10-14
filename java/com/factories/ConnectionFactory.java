66c2da14c0081dd13ad595e62321bbe1721ad4ac
Dhrupad Chakraborty <91726340+dhrupad17@users.noreply.github.com>
2023-10-14 12:07:32 +0530

    Update ConnectionFactory.java

--- java/com/factories/ConnectionFactory.java
+++ java/com/factories/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 	static {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb","root","root");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:portnumber/database","username","password");
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
